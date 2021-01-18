27b2d029e2da9364e5ab874bc82d65e9fb41ea6f
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2021-01-18 15:23:04 +0000

    Create BundleGenerator on top of DataBundle

---
+++ BundleGenerator.py
@@ -0,0 +1,49 @@
+import pykitti
+from os import listdir
+from glob import glob
+from DataBundle import DataBundle
+
+class BundleGenerator:
+    def __init__(self, base_dir):
+        self.dates = sorted(listdir(base_dir))
+        self.base_dir = base_dir
+
+    def load(self,date=None):
+        if date is None:
+            for date in self.dates:
+                return self._load_image_paths(date)
+
+        elif date in self.dates:
+            return self._load_image_paths(date)
+
+        else:
+            raise ValueError
+
+    def _load_image_paths(self, date):
+        drives = sorted(
+            list(
+                map(
+                    lambda x: x.split('_')[-2],
+                    filter(lambda x: not x.endswith('txt'), listdir(f'{self.base_dir}/{date}'))
+                )
+            )
+        )
+        calib = pykitti.raw(self.base_dir, date, drives[0]).calib
+        
+        img_paths = sorted(
+            sum(
+                map(
+                    lambda x: glob(
+                        f'{self.base_dir}/{date}/{date}_drive_{x}_sync/image_02/data/*.jpg'),
+                        drives
+                    ), []
+            )
+        )
+        
+        for img_path in img_paths:
+            yield DataBundle(img_path, calib)
+
+kitti_path = '/scratch/local/hdd/hizkia/kitti'
+a = BundleGenerator(kitti_path)
+
+n = input()
--- DataBundle.py
+++ DataBundle.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 class DataBundle:
-    def __init__(self, img_path):
+    def __init__(self, img_path, calib):
+        self.img_path = img_path
+        self.calib = calib
         self.image = cv2.imread(img_path)
         self.depth = np.load(img_path.replace('data', 'depth').replace('jpg','npz'))['depth']
         self.boxes = np.load(img_path.replace('image', 'panoptic').replace('jpg','npz'))['boxes']
         self.instances = np.load(img_path.replace('image', 'panoptic').replace('jpg','npz'))['instances']
-        self.img_path = img_path
 
     def plot_image(self):
         fig, ax = plt.subplots(1, figsize=(20,30))
@@ -35,12 +36,3 @@ class DataBundle:
         ax[2].imshow(self.instances)
 
         plt.show()
-
-kitti_path = '/scratch/local/hdd/hizkia/kitti'
-
-img_path = glob.glob(f'{kitti_path}/2011_09_26/*/image_02/data/*.jpg')
-
-ip = img_path[0]
-db = DataBundle(ip)
-
-n = input()
