c4395522425537668e2d775feedc1cf6dc63c327
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2021-02-07 18:22:52 +0000

    Expectation Maximization class

--- ExpectationOptimization.py
+++ ExpectationOptimization.py
@@ -1,9 +1,6 @@
-from CarGenerator import CarGenerator
+from CarGenerator import CarGenerator, Direction
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
-
-kitti_path = '/scratch/local/hdd/hizkia/kitti'
 
 def calculate_EM_score(normalized_samples, cov):
     inv_cov = np.linalg.inv(cov)
@@ -13,45 +10,66 @@ def _help(i, x):
     print(i,end='\r')
     return x.normalized_depth.ravel()
 
-# class ExpectationMaximization:
-#     def __init__(self, base_path, date=None, reference_rectangle=(64,128), min_original_rectangle=(16,32), max_iters=10):
-#         self.generator = CarGenerator(base_path, date=date, reference_rectangle=reference_rectangle, min_original_rectangle=min_original_rectangle, mean=None, cov=None).load_dataset()
+class ExpectationMaximization:
+    def __init__(
+        self, 
+        base_path, 
+        date=None, 
+        reference_rectangle=(64,128), 
+        min_original_rectangle=(32,64), 
+        max_iters=10):
 
+        self.base_path = base_path
+        self.date = date
+        self.reference_rectangle=reference_rectangle
+        self.min_original_rectangle=min_original_rectangle
+        self.max_iters = max_iters
 
-generator = CarGenerator(kitti_path, date=None, reference_rectangle=(64, 128), min_original_rectangle=(16,32), mean=None, cov=None).load_dataset()
-print("Generating samples...")
-D = np.array([_help(i,x) for i,x in enumerate(generator)]).T
+    def run(self):
+        generator = CarGenerator(
+            self.base_path, 
+            date=self.date, 
+            reference_rectangle=self.reference_rectangle, 
+            min_original_rectangle=self.min_original_rectangle, 
+            mean=None, 
+            cov=None).load_dataset()
+        print("Generating samples...")
+        D = np.array([_help(i,x) for i,x in enumerate(generator)]).T
 
-mean = D.mean(axis = 1)
-A = D - mean.reshape((-1,1))
-cov = np.cov(A)
+        mean = D.mean(axis = 1)
+        A = D - mean.reshape((-1,1))
+        cov = np.cov(A)
 
-score = calculate_EM_score(A, cov)
-np.savez("/scratch/local/hdd/hizkia/data_init.npz", mean=mean, cov=cov)
-print(f"Initializing EM yields a score of {score}...")
+        score = calculate_EM_score(A, cov)
+        print(f"Initializing EM yields a score of {score}...")
 
-num_of_iters = 10
-for iter_num in range(num_of_iters):
-    generator = CarGenerator(kitti_path, date=None, reference_rectangle=(64, 128), min_original_rectangle=(16,32), mean=mean, cov=cov).load_dataset()
-    print("Generating samples...")
-    D = np.array([_help(i,x) for i,x in enumerate(generator)]).T
+        for iter_num in range(self.max_iters):
+            optimize_direction = Direction(iter_num % 2)
+            generator = CarGenerator(
+                self.base_path,
+                date=self.date,
+                reference_rectangle=self.reference_rectangle,
+                min_original_rectangle=self.min_original_rectangle,
+                mean=mean,
+                cov=cov,
+                optimize_direction=optimize_direction
+            )
 
-    temp_mean = D.mean(axis = 1)
-    A = D - temp_mean.reshape((-1,1))
-    temp_cov = np.cov(A)
+            temp_D = np.array([_help(i,x) for i,x in enumerate(generator)]).T
 
-    temp_score = calculate_EM_score(A, temp_cov)
-    if temp_score > score:
-        break
+            temp_mean = temp_D.mean(axis=1)
+            temp_A = temp_D - temp_mean.reshape((-1,1))
+            temp_cov = np.cov(temp_A)
 
-    score = temp_score
-    mean = temp_mean
-    cov = temp_cov
-    np.savez(f"/scratch/local/hdd/hizkia/data_{iter_num}.npz", mean=mean, cov=cov)
-    print(f"EM iteration {iter_num} yields a score of {score}...")
-    
-print(f"EM stopped with a score of {score}. Saving data...")
+            temp_score = calculate_EM_score(temp_A, temp_cov)
+            if temp_score < score:
+                D = temp_D
+                mean = temp_mean
+                cov = temp_cov
+                score = temp_score
+                print(f"EM iteration {iter_num} yields a score of {score}...")
+            else:
+                break
 
-np.savez("/scratch/local/hdd/hizkia/data.npz", mean=mean, cov=cov, inv_cov = np.linalg.inv(cov))
-
-# print("Data saved successfully")
+        print(f"EM stopped with a score of {score}")
+        return D, mean, cov
