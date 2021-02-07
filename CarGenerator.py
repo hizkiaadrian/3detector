4d9b3f42643f5c142f038ec8aebd8c22ce638ad8
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2021-02-07 17:33:19 +0000

    Create directions for crop optimization

--- CarGenerator.py
+++ CarGenerator.py
@@ -5,6 +5,7 @@ from cv2 import resize, INTER_NEAREST
 from numpy import array, zeros, block, median
 from numpy.linalg import inv
 from math import ceil
+from enum import Enum
 
 def is_occluded(larger_box, smaller_box):
     return not (larger_box[2] <= smaller_box[0] or larger_box[0] >= smaller_box[2] or larger_box[3] <= smaller_box[1] or larger_box[1] >= smaller_box[3])
@@ -60,7 +61,12 @@ def get_iou(box1, box2):
     union = (box1[2]-box1[0]) * (box1[3]-box1[1]) + (box2[2]-box2[0]) * (box2[3]-box2[1]) - intersection
     return intersection / union
 
-def optimize_box(box, mean, inv_cov, depth, reference_rectangle, constraint_box, base_image_shape, min_intersection_ratio=0.95):
+class Direction(Enum):
+    VERTICAL = 0
+    HORIZONTAL = 1
+    ALL = 2
+
+def optimize_box(box, mean, inv_cov, depth, reference_rectangle, constraint_box, base_image_shape, direction=Direction.VERTICAL, min_intersection_ratio=0.8):
     crop = resize(
                     depth[box[0]:box[2], box[1]:box[3]], 
                     (reference_rectangle[1], reference_rectangle[0]), interpolation=INTER_NEAREST
@@ -70,29 +76,86 @@ def optimize_box(box, mean, inv_cov, depth, reference_rectangle, constraint_box,
 
     optimal_box = box.copy()
 
-    hw_ratio = reference_rectangle[0] / reference_rectangle[1]
-    for top_y in range(constraint_box[0]):
-        for top_x in range(constraint_box[1]):
-            for bottom_y in range(constraint_box[2], base_image_shape[0]):
-                bottom_x = top_x + (bottom_y - top_y) / hw_ratio
-                if not bottom_x.is_integer():
-                    continue
-                elif bottom_x < base_image_shape[1] and get_iou(box, [top_y, top_x, bottom_y, ceil(bottom_x)]) > min_intersection_ratio:
-                    crop = resize(
-                        depth[top_y:bottom_y, top_x:ceil(bottom_x)],
-                        (reference_rectangle[1], reference_rectangle[0]), interpolation=INTER_NEAREST
-                    )
-                    score = ((crop - median(crop)).ravel() - mean).T @ inv_cov @ ((crop - median(crop)).ravel() - mean)
-                    if score < min_score:
-                        min_score = score
-                        optimal_box = [top_y, top_x, bottom_y, ceil(bottom_x)]
-                else:
-                    break
+    if direction == Direction.VERTICAL:
+        for top_y in range(box[0], constraint_box[0]):
+            bottom_y = box[2] + top_y - box[0]
+            if bottom_y < base_image_shape[0] and get_iou(box, [top_y, box[1], bottom_y, box[3]]) > min_intersection_ratio:
+                crop = resize(
+                    depth[top_y:bottom_y, box[1]:box[3]],
+                    (reference_rectangle[1], reference_rectangle[0]), interpolation=INTER_NEAREST
+                )
+                score = ((crop - median(crop)).ravel() - mean).T @ inv_cov @ ((crop - median(crop)).ravel() - mean)
+                if score < min_score:
+                    min_score = score
+                    optimal_box = [top_y, box[1], bottom_y, box[3]]
+            else:
+                break
+        for top_y in range(box[0], -1, -1):
+            bottom_y = box[2] + top_y - box[0]
+            if bottom_y > constraint_box[2] and get_iou(box, [top_y, box[1], bottom_y, box[3]]) > min_intersection_ratio:
+                crop = resize(
+                    depth[top_y:bottom_y, box[1]:box[3]],
+                    (reference_rectangle[1], reference_rectangle[0]), interpolation=INTER_NEAREST
+                )
+                score = ((crop - median(crop)).ravel() - mean).T @ inv_cov @ ((crop - median(crop)).ravel() - mean)
+                if score < min_score:
+                    min_score = score
+                    optimal_box = [top_y, box[1], bottom_y, box[3]]
+            else:
+                break
+
+    elif direction == Direction.HORIZONTAL:
+        for top_x in range(box[1], constraint_box[1]):
+            bottom_x = box[3] + top_x - box[1]
+            if bottom_x < base_image_shape[1] and get_iou(box, [box[0], top_x, box[2], bottom_x]) > min_intersection_ratio:
+                crop = resize(
+                    depth[box[0]:box[2], top_x:bottom_x],
+                    (reference_rectangle[1], reference_rectangle[0]), interpolation=INTER_NEAREST
+                )
+                score = ((crop - median(crop)).ravel() - mean).T @ inv_cov @ ((crop - median(crop)).ravel() - mean)
+                if score < min_score:
+                    min_score = score
+                    optimal_box = [box[0], top_x, box[2], bottom_x]
+            else:
+                break
+        for top_x in range(box[1], -1, -1):
+            bottom_x = box[3] + top_x - box[1]
+            if bottom_x > constraint_box[3] and get_iou(box, [box[0], top_x, box[2], bottom_x]) > min_intersection_ratio:
+                crop = resize(
+                    depth[box[0]:box[2], top_x:bottom_x],
+                    (reference_rectangle[1], reference_rectangle[0]), interpolation=INTER_NEAREST
+                )
+                score = ((crop - median(crop)).ravel() - mean).T @ inv_cov @ ((crop - median(crop)).ravel() - mean)
+                if score < min_score:
+                    min_score = score
+                    optimal_box = [box[0], top_x, box[2], bottom_x]
+            else:
+                break
+
+    elif direction == Direction.ALL:
+        hw_ratio = reference_rectangle[0] / reference_rectangle[1]
+        for top_y in range(constraint_box[0]):
+            for top_x in range(constraint_box[1]):
+                for bottom_y in range(constraint_box[2], base_image_shape[0]):
+                    bottom_x = top_x + (bottom_y - top_y) / hw_ratio
+                    if not bottom_x.is_integer():
+                        continue
+                    elif bottom_x < base_image_shape[1] and get_iou(box, [top_y, top_x, bottom_y, ceil(bottom_x)]) > min_intersection_ratio:
+                        crop = resize(
+                            depth[top_y:bottom_y, top_x:ceil(bottom_x)],
+                            (reference_rectangle[1], reference_rectangle[0]), interpolation=INTER_NEAREST
+                        )
+                        score = ((crop - median(crop)).ravel() - mean).T @ inv_cov @ ((crop - median(crop)).ravel() - mean)
+                        if score < min_score:
+                            min_score = score
+                            optimal_box = [top_y, top_x, bottom_y, ceil(bottom_x)]
+                    else:
+                        break
 
     return optimal_box
 
 class CarGenerator:
-    def __init__(self, base_dir, date = None, reference_rectangle = (64, 128), min_original_rectangle = (16, 32), mean = None, cov = None):
+    def __init__(self, base_dir, date = None, reference_rectangle = (64, 128), min_original_rectangle = (16, 32), mean = None, cov = None, optimize_direction = Direction.ALL):
         if date is not None and date not in listdir(base_dir):
             raise ValueError
         self._bg_object = BundleGenerator(base_dir)
@@ -102,6 +165,7 @@ class CarGenerator:
         self.mean = mean
         self.cov = cov
         self.inv_cov = inv(cov) if cov is not None else None
+        self.optimize_direction = optimize_direction
 
     def load_dataset(self):
         for bundle in self.bundle_generator:
@@ -114,9 +178,16 @@ class CarGenerator:
                 loosened_box = loosen_box(box, self.reference_rectangle, bundle.image.shape)
                 if not loosened_box:
                     continue
-
                 if (self.mean is not None) and (self.inv_cov is not None):
-                    optimized_box = optimize_box(loosened_box, self.mean, self.inv_cov, bundle.depth, self.reference_rectangle, box, bundle.image.shape)
+                    optimized_box = optimize_box(
+                        box=loosened_box, 
+                        mean=self.mean,
+                        inv_cov=self.inv_cov, 
+                        depth=bundle.depth, 
+                        reference_rectangle=self.reference_rectangle, 
+                        constraint_box=box, 
+                        base_image_shape=bundle.image.shape, 
+                        direction=self.optimize_direction)
                 else:
                     optimized_box = loosened_box
 
