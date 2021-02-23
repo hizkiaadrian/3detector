ac84d9368d72f56005d8cc2abb72703363b94beb
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2021-02-23 15:52:05 +0000

    Change CarMesh initial sphere to built-in pytorch sphere mesh

--- MeshFitter/CarMesh.py
+++ MeshFitter/CarMesh.py
@@ -1,16 +1,20 @@
 import numpy as np
 import plotly.graph_objects as go
+import torch
+from pytorch3d.utils import ico_sphere
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+else:
+    device = torch.device("cpu")
 
 class CarMesh:
-    def __init__(self, center = 0, radius = 5):
-        self.vertices = np.array([
-            [radius * np.sin(azimuth) * np.cos(theta), 
-             radius * np.sin(azimuth) * np.sin(theta),
-             radius * np.cos(azimuth)] 
-             for azimuth in np.linspace(0,np.pi,60) for theta in np.linspace(0,2*np.pi,60)]
-            )
+    def __init__(self):
+        self.icosphere = ico_sphere(4, device)
 
     def plot(self):
-        x,y,z = self.vertices.T
-        fig = go.Figure(data=[go.Mesh3d(x=x, y=y, z=z, alphahull=0, colorscale=[[0, 'gold'], [1, 'magenta']], intensity=np.linspace(0,1,num=x.shape[0]))])
+        x,y,z = self.icosphere.verts_packed().cpu().numpy().T
+        i,j,k = self.icosphere.faces_packed().cpu().numpy().T
+        fig = go.Figure(data=[go.Mesh3d(x=x, y=y, z=z, i=i, j=j, k=k, colorscale=[[0, 'gold'], [1, 'magenta']], intensity=y)])
         return fig
