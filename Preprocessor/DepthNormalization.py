9252e6493be0d0c0f2e6e30e0f88e4230a639de2
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2021-02-24 15:01:50 +0000

    Change definition of masked normalization

--- Preprocessor/DepthNormalization.py
+++ Preprocessor/DepthNormalization.py
@@ -7,15 +7,15 @@ def subtract_median(depth, instance):
 def subtract_median_masked(depth, instance):
     instance_num = _get_instance_num(instance)
     depth_median = median(depth[instance == instance_num])
-    maskval = (max(depth[instance == instance_num]) - depth_median) * 1.1
-    return vectorize(lambda d, i: d - depth_median if i == instance_num else maskval)(depth, instance)
+    return vectorize(lambda d, i: d - depth_median if i == instance_num else -1)(depth, instance)
 
 def divide_median(depth, instance):
     return depth / median(depth)
 
 def divide_median_masked(depth, instance):
     instance_num = _get_instance_num(instance)
-    return multiply(depth, instance == instance_num) / median(depth[instance == instance_num])
+    depth_median = median(depth[instance == instance_num])
+    return vectorize(lambda d, i: d - depth_median if i == instance_num else -1)(depth, instance)
 
 def rescale_minmax(depth, instance):
     return (depth - depth.min()) / (depth.max() - depth.min())
@@ -24,7 +24,7 @@ def rescale_minmax_masked(depth, instance):
     instance_num = _get_instance_num(instance)
     car_depth = depth[instance == instance_num]
     minval, maxval = min(car_depth), max(car_depth)
-    return vectorize(lambda x, y: (x-minval)/(maxval-minval) if y == instance_num else 1.1)(depth, instance)
+    return vectorize(lambda x, y: (x-minval)/(maxval-minval) if y == instance_num else -1)(depth, instance)
 
 def _get_instance_num(instance):
     return mode(instance[instance != 0])[0][0]
